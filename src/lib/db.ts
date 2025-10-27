6b539272fb75df33dc0a4fea09aecd598d91db73
Jenil Rupapara <83698234+jenilrupapara001@users.noreply.github.com>
2025-10-27 13:17:48 +0530

    Refactor PrismaClient initialization and logging

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,13 +1,11 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
-}
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
 export const db =
-  globalForPrisma.prisma ??
+  globalForPrisma.prisma ||
   new PrismaClient({
-    log: ['query'],
-  })
+    log: ["error", "warn"],
+  });
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
